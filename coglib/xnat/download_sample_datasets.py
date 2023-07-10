db48a8953ef819c2f09cafa68748f2783566d721
Praveen Sripad <pravsripad@gmail.com>
2023-07-10 08:23:37 +0200

    Add script to download sample dataset.

---
+++ coglib/xnat/download_sample_datasets.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# author: Praveen Sripad <praveen.sripad@ae.mpg.de>
+
+"""Simple script uses xnatpy to download all or part of the Cogitate Sample Dataset.
+
+The cogitate sample dataset consists of data shared from their original projects.
+
+Two subjects from every site are included:
+FMRI - SC102, SC202, SD101, SD199
+MEEG - SA124, SA140, SB013, SB072
+ECOG - SE107, SE110, SF102, SF104
+
+"""
+
+import os
+import os.path as op
+import xnat
+
+
+def _download_resources_recursively(xnat_object, download_dir):
+    """Download xnat resources recursively for projects, subjects and experiments.
+
+    Scan level data/resources are not downloaded.
+
+    """
+    print('Downloaded data for %s into directory %s'
+          % (xnat_object, download_dir))
+
+    if not op.isdir(download_dir):
+        os.mkdir(download_dir)
+
+    resources_list = xnat_object.resources.listing
+
+    if len(resources_list) != 0:
+        for res in resources_list:
+            print(res)
+            res.download_dir(download_dir)
+
+    # if xnat object is project, recurse
+    if type(xnat_object).__name__ == 'ProjectData':
+        for subj in xnat_object.subjects.listing:
+            _download_resources_recursively(subj,
+                                            op.join(download_dir, subj.label))
+
+    # if xnat object is subject, recurse
+    if type(xnat_object).__name__ == 'SubjectData':
+        for exp in xnat_object.experiments.listing:
+            _download_resources_recursively(exp, op.join(download_dir,
+                                            xnat_object.label, exp.label))
+
+    return
+
+
+# CONFIG
+xnat_host = 'https://xnat-curate.ae.mpg.de'
+netrc_file = op.join(op.expanduser('~'), '.xnat_curate_netrc')
+
+myproject = 'cogitate_sample_dataset'
+download_dir = op.join(op.expanduser('~'), 'Downloads')
+# CONFIG
+
+# start connection to XNAT
+connection = xnat.connect(xnat_host, netrc_file=netrc_file)
+
+# choose the project
+project = connection.projects[myproject]
+
+# download everything under the project
+project.download_dir(download_dir)
+
+# download all the data for a subject
+# mysubject = 'SA124'
+# project.subjects[mysubject].download_dir(download_dir)
+
+# download all the resources only (excluding the scans)
+# _download_resources_recursively(project, download_dir)
